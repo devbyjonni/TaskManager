b0b7388a0b09594c30a3f87467325ca1769057ce
Jonni Åkesson <enterthesource@me.com>
2024-09-08 21:22:38 +0200

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,18 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Jonni Akesson on 2024-09-08.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+        //.modelContainer(for: Task.self)
+    }
+}
